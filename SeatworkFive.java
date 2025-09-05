004bb272ac1c1b466bbfcee280a4725e48feaa1a
mariakristinafhatimaatonscc-lgtm <mariakristinafhatimaaton.scc@gmail.com>
2025-09-05 10:22:11 +0000

    Done

---
+++ SeatworkFive.java
@@ -0,0 +1,14 @@
+public class SeatworkFive {
+    public static void main(String[] args) {
+
+        System.out.println("Maria Kristina Fhatima I. Aton");
+        int countdown = 3;
+
+        while (countdown > 0 ) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy new year!");
+    }
+}
