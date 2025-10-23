bed3535dd0e23ab6f408903a193675f8fad55bf1
mariakristinafhatimaatonscc-lgtm <mariakristinafhatimaaton.scc@gmail.com>
2025-10-23 04:26:44 +0000

    SW14

---
+++ SeatworkFourteen.java
@@ -0,0 +1,36 @@
+public class Motor {
+    public static void main(String args[]) {
+        Motorcycle m = new Motorcycle();
+        m.make = "Yamaha RZ350";
+        m.color = "yellow";
+
+        System.out.println("Calling showAtts...");
+        m.showAtts();
+        System.out.println("-------");
+
+        System.out.println("Starting engine...");
+        m.startEngine();
+        System.out.println("-------");
+
+        System.out.println("Calling showAtts...");
+        m.showAtts();
+        System.out.println("-------");
+
+        System.out.println("Starting engine...");
+        m.startEngine();
+    }
+}
+
+class Motorcycle {
+    String make;
+    String color;
+
+    void showAtts() {
+        System.out.println("Make: " + make);
+        System.out.println("Color: " + color);
+    }
+
+    void startEngine() {
+        System.out.println(make + " engine started!");
+    }
+}
